df90f4883d724d6ecb85b82878cdf626a5ccf49e
Liang Kun <liangkun@outlook.com>
2025-03-23 21:44:06 +0800

    setup basic program structure

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# maria
-An agent playing games. 
+# Mario
+一个会玩游戏的AI Agent。蓓蓓练习使用的小项目。
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="mario",
+    version="0.9",
+    author="liangyu",
+    author_email="liangyu@math.com",
+    description="An agent playing games.",
+
+    url="https://github.com/liangkun/mario", 
+
+    packages=find_packages()
+)
